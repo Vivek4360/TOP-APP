4c77bf078dcc288741c94f662fd7b52dc10812b3
Vivek <Vkhindu628465@gmail.com>
2025-01-21 19:23:09 +0530

    TOP Project  Updated

--- TOP/ContentView.swift
+++ TOP/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello,Vinod Welcome to TheOutsourcePro")
+            Text("Hello,Vinod Welcome to TheOutsourcePro MY First SwiftUI App Thank you Xcode")
         }
         .padding()
     }
